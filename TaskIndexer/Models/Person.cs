fff7077e121e719e989b095c69c0b6f461ff58f3
unknown <tu7pgums7@code.edu.az>
2022-03-25 06:00:21 +0400

    program.cs

---
+++ TaskIndexer/Models/Person.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TaskIndexer.Models
+{
+    public class Person
+    {
+        private string[] person = new string[10];
+
+        private string[] _name;
+
+
+
+        public string this[int i]
+        {
+            get
+            {
+                return _name[i];
+            }
+            set
+            {
+                _name[i] = value;
+            }
+        }
+        private string[] _surname;
+        public string this[int y, string x]
+        {
+            get
+            {
+                return _surname[y];
+
+            }
+            set
+            {
+                _surname[y] = value;
+            }
+        }
+        private int[] _age;
+        public int this[int a, bool d]
+        {
+            get
+            {
+                return _age[a];
+            }
+            set
+            {
+                _age[a] = value;
+            }
+        }
+        public Person()
+        {
+            _name = new string[6];
+           _surname = new string[6];
+            _age = new int[6];
+
+
+
+        }
+    }
+}
+        
+        
+
---
+++ TaskIndexer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskIndexer.Models;
+
+namespace TaskIndexer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person[0] = "Aysel, Mirze, 19";
+            person[1] = "Nigar, Aliyeva 22";
+            person[2] = " Ali, Aliyev, 18";
+            person[3] = "Isa, Rzayev, 21";
+            person[4] = "Sema, Mahmudova 15";
+            person[5] = "Leyla, Memmedova 25";
+
+
+            for (int i = 0; i < 6; i++)
+                Console.WriteLine(person[i]);
+        }
+    }
+}
